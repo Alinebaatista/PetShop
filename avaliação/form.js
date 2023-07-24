1a5c0a38ce82384e549ef3feee66eca253b6cd34
Aline Batista <102964288+Alinebaatista@users.noreply.github.com>
2023-07-24 19:55:53 -0300

    Add files via upload

---
+++ avaliação/form.js
@@ -0,0 +1,39 @@
+
+
+function mensagem(){
+    alert('COMPRA REALIZADA');
+
+
+
+
+    var captar = document.getElementById('name').value;
+    document.getElementById('nameR').innerHTML = captar;
+
+
+var captar2 = 0
+
+    captar2 = document.getElementById('CPF').value;
+    document.getElementById('CPF1').innerHTML = captar2;
+
+var captar3 = 0
+
+    captar3 = document.getElementById('email').value;
+    document.getElementById('email1').innerHTML = captar3;
+    
+ 
+
+ var captar4 = document.getElementById('FormaPag').value;
+document.getElementById('FormaP').innerHTML = captar4;
+
+
+
+var captar5 = document.getElementById('endereco').value;
+document.getElementById('enderecoC').innerHTML = captar5;
+
+
+var captar6 = document.getElementById('frete1').value;
+document.getElementById('Frete').innerHTML = captar6;
+
+
+
+}
